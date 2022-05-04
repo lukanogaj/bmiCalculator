ec31a492395575ce5367906319863e4e92097098
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2022-05-04 17:53:14 +0100

    updateunits

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// Select the elements
-let btnShowEngUnits = document.querySelector('#switchToEngMes');
-let englishHeightUnits = document.querySelector('#english-height');
+'use strict';
 
-// Add event listeners to the button
-btnShowEngUnits.addEventListener('click', () => {
+// Select the elements
+// The button and switch for height
+const btnShowEngUnitsHeight = document.querySelector('#switchToEngMes');
+const englishHeightUnits = document.querySelector('#english-height');
+// The button and switch for the weight
+const btnShowEngUnitsWeight = document.querySelector('#switchToEngMesWeight');
+const englishWeightUnits = document.querySelector('#english-weight');
+
+// Add event listeners to the button for the height /hide show div
+btnShowEngUnitsHeight.addEventListener('click', () => {
   englishHeightUnits.classList.toggle('reveal');
   console.log('hello');
 });
 
+// Add event listener to the button for weight /hide and show
+
+btnShowEngUnitsWeight.addEventListener('click', () => {
+  englishWeightUnits.classList.toggle('reveal-weightEng');
+  console.log('hello');
+});
+
 // Console logs
-console.log(btnShowEngUnits, englishHeightUnits);
+console.log(btnShowEngUnitsHeight, englishHeightUnits);
